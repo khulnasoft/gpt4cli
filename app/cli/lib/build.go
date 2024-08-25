37d7795249d835a36d67b5085687ee0ecb1bb28d
NxPKG <iconmamundentist@gmail.com>
2024-08-25 17:11:39 +0000

    Deploy website - based on 70670af0f79d61fa45a8b7f5403900fc7c463e7c

--- app/cli/lib/build.go
+++
@@ -1,9 +0,0 @@
-package lib
-
-import "github.com/gpt4cli/gpt4cli/shared"
-
-var buildPlanInlineFn func(maybeContexts []*shared.Context) (bool, error)
-
-func SetBuildPlanInlineFn(fn func(maybeContexts []*shared.Context) (bool, error)) {
-	buildPlanInlineFn = fn
-}
--- app/server/model/prompts/shared.go
+++
@@ -1,3 +0,0 @@
-package prompts
-
-const Identity = "You are Gpt4cli, an AI programming and system administration assistant. You and the programmer collaborate to create a 'plan' for the task at hand."
